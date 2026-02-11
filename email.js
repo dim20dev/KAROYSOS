03a7517e8fb58df25ff1af8a79282077c4edb54a
Noah <dimitrisntzagidis@gmail.com>
2026-02-11 14:22:09 +0200

    Delete email.js

--- email.js
+++
@@ -1,7 +0,0 @@
-
-function sendEmail(){
-let orders=dbGet('orders');
-let body="Παραγγελία:\n";
-orders.forEach(o=>body+=o.product+" "+o.qty+"\n");
-window.location.href="mailto:?subject=Order&body="+encodeURIComponent(body);
-}
